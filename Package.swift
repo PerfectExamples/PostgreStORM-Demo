deda01444751f5a84f4aab95f172734f19fd01a0
Jonathan Guthrie <jono@perfect.org>
2017-10-03 15:36:55 -0400

    update for swift 4/perfect 3

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-04 16:25:29 +0000
+// Date: 2017-10-03 19:30:36 +0000
 import PackageDescription
 let package = Package(
-    name: "PostgreStORM-Demo",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 1, minor: 0),
-    ]
+	name: "PostgreStORM-Demo",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 3),
+	]
 )
